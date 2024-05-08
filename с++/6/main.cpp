0591c93549001e4b4103b1fdbcacc6ce82d1b4e6
NastyaKun <148327680+NastyaKun@users.noreply.github.com>
2024-05-08 16:19:48 +0200

    Create main.cpp

---
+++ с++/6/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <SFML/Graphics.hpp>
+#include <Circle.hpp>
+#include <Game.hpp>
+
+int main()
+{
+    mt::Game game(1100, 1000, "Game");
+
+    if (!game.Setup(5)) 
+    {
+        return -1;
+    }
+
+    game.Life();
+
+    return 0;
+}
