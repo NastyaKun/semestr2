9557a1991955db7a55cb1e282a4c03456628f9ec
NastyaKun <148327680+NastyaKun@users.noreply.github.com>
2024-05-25 09:45:03 +0200

    Update main.cpp

--- с++/6/main.cpp
+++ с++/6/main.cpp
@@ -6,12 +6,10 @@ int main()
 {
     mt::Game game(1100, 1000, "Game");
 
-    if (!game.Setup(5)) 
-    {
+    if (!game.Setup(10))
         return -1;
-    }
 
-    game.Life();
+    game.LifeCycle();
 
     return 0;
 }
