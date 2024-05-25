c87ea0d31e2ba0f0522bdd89ba24d157e70beb7f
NastyaKun <148327680+NastyaKun@users.noreply.github.com>
2024-05-25 09:44:55 +0200

    Update Game.hpp

--- с++/6/inc/Game.hpp
+++ с++/6/inc/Game.hpp
@@ -6,9 +6,12 @@
 #include <Ship.hpp>
 #include <Circle.hpp>
 
-namespace mt 
+namespace mt
 {
-	class Game 
+
+	const float pi = acos(-1);
+
+	class Game
 	{
 		int m_width;
 		int m_height;
@@ -16,50 +19,48 @@ namespace mt
 		mt::Circle* m_c;
 		int m_n;
 		sf::RenderWindow m_window;
-		sf::Texture m_TextureBackground;
-		sf::Sprite m_SpriteBackground;
-
-		Ship m_Ship;
+		sf::Texture m_textureBackground;
+		sf::Sprite m_spriteBackground;
+		sf::Font m_font;
+		Ship m_ship;
 
 	public:
-		Game(int width, int height, const std::string& capture) 
+		Game(int width, int height, const std::string& capture)
 		{
 			m_width = width;
 			m_height = height;
 			m_capture = capture;
 		}
 
-		bool Setup(int n) 
+		bool Setup(int n)
 		{
 			m_n = n;
-			
-			//Создание окна
+			// Создание окна
 			m_window.create(sf::VideoMode(m_width, m_height), m_capture);
-			m_window.setTitle(m_capture);
 
-			//Загрузка текстуры фона
-			if (!m_TextureBackground.loadFromFile("assets\\Background.jpg")) 
+			// Создание текстуры фона
+			if (!m_textureBackground.loadFromFile("assets\\background.jpg"))
 			{
-				std::cout << "Error while loading background.jpg";
+				std::cout << "Error while loading background.jpg" << std::endl;
 				return false;
 			}
-			m_SpriteBackground.setTexture(m_TextureBackground);
-			
-			//Загрузка коробля игрока
-			if (!m_Ship.Setup(100, 100)) 
+			m_spriteBackground.setTexture(m_textureBackground);
+
+			// Загрузка коробля игрока
+			if (!m_ship.Setup(100, 100))
 				return false;
-			
+
+			srand(time(0));
 
 			m_c = new mt::Circle[m_n];
-			srand(time(0));
-			for (int i = 0; i < m_n; i++) 
+			for (int i = 0; i < m_n; i++)
 			{
-				int r = rand() % 200 + 1;
-				int x = rand() % (1100 - 2 * r) + r;
-				int y = rand() % (1000 - 2 * r) + r;
-				int dx = (rand() % 200);
-				int dy = (rand() % 200);
-				m_c[i].Setup(x, y, r, dx, dy);
+				int r = rand() % 100 + 1;
+				int x = rand() % (1000 - 2 * r) + r;
+				int y = rand() % (600 - 2 * r) + r;
+				int v = rand() % 50 + 10;
+				float alfa = rand() % 7;
+				m_c[i].Setup(x, y, r, v, alfa);
 			}
 		}
 
@@ -67,82 +68,77 @@ namespace mt
 		{
 			float x = obj.X();
 			float y = obj.Y();
-			float dx1 = obj.dx();
-			float dy1 = obj.dy();
 			float r = obj.R();
 
-			if (x + r >= m_width || x - r <= 0) 
-				obj.dx(obj.dx());
-			
-			if (y + r >= m_height || y - r <= 0) 
-				obj.dy(obj.dy());
+			if (x + r >= m_width || x - r <= 0)
+			{
+				obj.Alfa(pi - obj.Alfa());
+			}
+
+			if (y + r >= m_height || y - r <= 0)
+			{
+				obj.Alfa(2 * pi - obj.Alfa());
+			}
+
 		}
 
 
-		void Life()
+		void LifeCycle()
 		{
 			sf::Clock clock;
 
+
 			while (m_window.isOpen())
 			{
-				//Обработка событий
+				// Обработка событий
 				sf::Event event;
 				while (m_window.pollEvent(event))
 				{
-					if (event.type == sf::Event::Closed) 
+					if (event.type == sf::Event::Closed)
 						m_window.close();
 				}
 
-				//Обработка клавиатуры
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) 
+				// Обработка клавиатуры
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 				{
-					m_Ship.SetVelocity(0.001);
+					m_ship.setVelocity(0.0001);
 				}
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) 
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 				{
-					m_Ship.SetVelocity(-0.001);
+					m_ship.setVelocity(-0.0001);
 				}
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) 
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 				{
-					m_Ship.Rotate(-0.1);
+					m_ship.Rotate(-0.1);
 				}
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) 
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 				{
-					m_Ship.Rotate(0.1);
+					m_ship.Rotate(0.1);
 				}
 
-				
-				//Логика
+				// Логика
 				float dt = clock.getElapsedTime().asSeconds();
 				clock.restart();
 
-				//Перемещение шарика
-				for (int i = 0; i < m_n; i++) 
-				{
+				// Перемещение шарика
+				for (int i = 0; i < m_n; i++)
 					m_c[i].Move(dt);
-				}
-				for (int i = 0; i < m_n; i++) 
-				{
+
+				for (int i = 0; i < m_n; i++)
 					TouchBorder(m_c[i]);
-				}
 
-				m_Ship.Move();
+				// Перемещение коробля
+				m_ship.Move();
 
-				//Отборажение
+				// Отображение
 				m_window.clear();
-				m_window.draw(m_SpriteBackground);
-				m_window.draw(m_Ship.Get());
-				for (int i = 0; i < m_n; i++) 
-				{
+				m_window.draw(m_spriteBackground);
+				m_window.draw(m_ship.Get());
+				for (int i = 0; i < m_n; i++)
 					m_window.draw(m_c[i].Get());
-				}
 				m_window.display();
 			}
 		}
-
-		~Game()
-		{
-			delete[] m_c;
-		}
 	};
+
 }
