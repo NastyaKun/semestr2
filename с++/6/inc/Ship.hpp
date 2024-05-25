8405eb542be55e67b7ecc74dc49a59e90c9a9f2f
NastyaKun <148327680+NastyaKun@users.noreply.github.com>
2024-05-25 09:44:59 +0200

    Update Ship.hpp

--- с++/6/inc/Ship.hpp
+++ с++/6/inc/Ship.hpp
@@ -2,69 +2,70 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-
-namespace mt 
+namespace mt
 {
-	class Ship 
+
+	class Ship
 	{
-		float m_x, m_y, m_v;
+		float m_x, m_y;
 		float m_alpha;
-		sf::Texture m_TextureShip;
-		sf::Sprite m_SpriteShip;
+		float m_v;
+		sf::Texture m_textureShip;
+		sf::Sprite m_spriteShip;
 
 	public:
 		Ship() = default;
-
-		Ship(float x, float y) 
+		
+		Ship(float x, float y)
 		{
 			m_x = x;
 			m_y = y;
 		}
 
-		bool Setup(float x, float y) 
+		bool Setup(float x, float y)
 		{
 			m_x = x;
 			m_y = y;
 			m_alpha = 0;
 			m_v = 0;
 
-			//Загрузка текстуры
-			if (!m_TextureShip.loadFromFile("assets\\textureShip1.jpg")) 
+			// Загрузка текстуры коробля
+			if (!m_textureShip.loadFromFile("assets\\textureShip1.jpg"))
 			{
-				std::cout << "Error while loading textureShip1.jpg";
+				std::cout << "Error while loading falcon.png" << std::endl;
 				return false;
 			}
+			m_spriteShip.setTexture(m_textureShip);
 
-			m_SpriteShip.setTexture(m_TextureShip);
-
-			m_SpriteShip.setScale(0.2f, 0.2f);
-			m_SpriteShip.setOrigin(m_TextureShip.getSize().x / 2, m_TextureShip.getSize().y / 2);
-			m_SpriteShip.setPosition(m_x, m_y);
-			m_SpriteShip.setRotation(m_alpha);
+			m_spriteShip.setScale(0.2f, 0.2f);
+			m_spriteShip.setOrigin(m_textureShip.getSize().x / 2, m_textureShip.getSize().y / 2);
+			m_spriteShip.setPosition(m_x, m_y);
+			m_spriteShip.setRotation(m_alpha);
 		}
 
-		void SetVelocity(float dv) 
+		void setVelocity(float dv)
 		{
 			m_v += dv;
 		}
 
-		void Move() 
+		void Move()
 		{
-
-			float AlphaRad = acos(-1) * m_alpha / 180;
-			m_x += m_v * cos(AlphaRad);
-			m_y += m_v * sin(AlphaRad);
-			m_SpriteShip.setPosition(m_x, m_y);
+			float alphaRad = acos(-1) * m_alpha / 180;
+			m_x += m_v * cos(alphaRad);
+			m_y += m_v * sin(alphaRad);
+			m_spriteShip.setPosition(m_x, m_y);
 		}
 
-		void Rotate(float dalpha) {
+		void Rotate(float dalpha)
+		{
 			m_alpha += dalpha;
-			m_SpriteShip.setRotation(m_alpha);
+			m_spriteShip.setRotation(m_alpha);
 		}
 
-		sf::Sprite Get() 
+		sf::Sprite Get()
 		{
-			return m_SpriteShip;
+			return m_spriteShip;
 		}
 	};
+
 }
